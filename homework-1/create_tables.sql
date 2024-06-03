97d09bfb59734724fcbd7a13b0f17d29ca1b7762
MARAT <almat59@rambler.ru>
2024-06-03 11:52:11 +0300

    HW_1

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,26 @@
 -- SQL-команды для создания таблиц
+CREATE DATABASE north;
+CREATE TABLE orders(
+	order_id int PRIMARY KEY NOT NULL,
+	customer_id varchar(7) NOT NULL,
+	employee_id varchar(15) NOT NULL,
+	order_date date NOT NULL,
+	ship_city text NOT NULL
+);
+
+CREATE TABLE customers_data(
+	customer_id varchar(7) PRIMARY KEY NOT NULL,
+	company_name varchar(50) NOT NULL,
+	contact_name varchar(50) NOT NULL
+);
+
+CREATE TABLE employees_data(
+	employee_id int PRIMARY KEY NOT NULL,
+	first_name varchar(15) NOT NULL,
+	last_name varchar(50) NOT NULL,
+	title varchar(500) NOT NULL,
+	birth_date varchar(15) NOT NULL,
+	notes text NOT NULL
+);
+
+ALTER TABLE orders RENAME TO orders_data;
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,26 @@
-"""Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+import csv
+
+conn = psycopg2.connect(
+    host='localhost',
+    database='north',
+    user='postgres',
+    password='121'
+)
+
+cur = conn.cursor()
+
+# Заполнение таблиц данными из файлов
+paths = ['C:/Users/MARAT/PycharmProjects/postgres-homeworks/homework-1/north_data/customers_data.csv',
+         "C:/Users/MARAT/PycharmProjects/postgres-homeworks/homework-1/north_data/employees_data.csv",
+         "C:/Users/MARAT/PycharmProjects/postgres-homeworks/homework-1/north_data/orders_data.csv"]
+
+for path in paths:
+    with open(path, 'r') as file:
+        next(file)  # Пропускаем заголовок
+        table_name = path.split('/')[-1].split('.')[0]  # Получаем имя таблицы из имени файла
+        cur.copy_from(file, table_name, sep=';')
+
+# Фиксация изменений и закрытие соединения
+conn.commit()
+conn.close()
